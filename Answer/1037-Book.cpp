919fcd84704af9da70dec4b612d8539006969d92
ohmusan 3 <ohmnotbad@gmail.com>
2024-12-16 01:58:37 +0700

    Update 1037-Book.cpp

--- Answer/1037-Book.cpp
+++ Answer/1037-Book.cpp
@@ -2,28 +2,19 @@
 using namespace std;
 
 bool fuc(string &s, int s_be, int s_size) {
-    if (s_be >= s_size) {
-        return true;
-    }
-
-    if (s[s_be] != s[s_size]) {
-        return false;
-    }
-
+    if (s_be >= s_size) return true;
+    if (s[s_be] != s[s_size]) return false;
+    
     return fuc(s, s_be + 1, s_size - 1);
 }
 
 int main() {
     string s;
-
     cin >> s;
 
-    if (fuc(s, 0, s.size() - 1)) {
-        cout << "yes";
-    } else {
-        cout << "no";
-    }
-
+    if (fuc(s, 0, s.size() - 1)) cout << "yes";
+    else cout << "no";
+    
     return 0;
 }
 
