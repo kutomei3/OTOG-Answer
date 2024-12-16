e0af68ea198e738d38b16470471b0717713293aa
ohmusan 3 <ohmnotbad@gmail.com>
2024-12-16 18:35:42 +0700

    Create 31-สี่สหายทลายซอมบี้.cpp

---
+++ Answer/31-สี่สหายทลายซอมบี้.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> sold(n);
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> sold[i];
+        sum += sold[i];
+    }
+
+    sort(sold.rbegin(), sold.rend());
+    //same as sort(sold.begin(), sold.end(), greater<int>());
+
+    n /= 4;
+    for (int i = 1; i <= n; i++) sum -= sold[i * 2 - 1];
+
+    cout << sum << '\n';
+
+    return 0;
+}
