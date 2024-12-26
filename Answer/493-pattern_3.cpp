df6ca89dd39c51c8ee6e190aa1cf99ed81f2c728
ohmusan <ohmnotbad@gmail.com>
2024-12-27 06:01:20 +0700

    Update 493-pattern_3.cpp

--- Answer/493-pattern_3.cpp
+++ Answer/493-pattern_3.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << '-';
+        }
+        for (int j = 0; j < n - i; j++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
