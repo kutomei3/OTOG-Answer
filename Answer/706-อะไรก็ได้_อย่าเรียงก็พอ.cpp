6a309654320e2a07424d1b6b03e789c3dc799312
ohmusan <ohmnotbad@gmail.com>
2024-12-28 03:26:28 +0700

    Create 706-อะไรก็ได้_อย่าเรียงก็พอ.cpp

---
+++ Answer/706-อะไรก็ได้_อย่าเรียงก็พอ.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cout << "2 2 1 0";
+
+    return 0;
+}
+
+//bad problem
+
