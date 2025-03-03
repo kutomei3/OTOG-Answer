7d4b4312ce623b8eb9d017543dd55e143e12e24e
ohmusan <ohmnotbad@gmail.com>
2025-03-03 07:11:04 +0700

    Update 593-sort_2.cpp

--- Answer/593-sort_2.cpp
+++ Answer/593-sort_2.cpp
@@ -1,27 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+#define int long long
+
+signed main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    int n, v;
+    int n;
     cin >> n;
 
-    vector<int> od, ev;
+    vector<int> vec(n);
+    for (auto& p : vec) cin >> p;
 
-    for (int i = 0; i < n; i++) {
-        cin >> v;
-        if (v & 1) od.push_back(v);
-        else ev.push_back(v); 
-    }
+    sort(vec.begin(), vec.end(), [](int a, int b) {
+        if ((a & 1) == (b & 1)) return a & 1 ? a > b : a < b;
+        return (a & 1) < (b & 1);
+    });
 
-    sort(od.rbegin(), od.rend());
-    sort(ev.begin(), ev.end());
-
-    for (auto& p : ev) cout << p << ' ';
-    for (auto& p : od) cout << p << ' ';
+    for (auto& p : vec) cout << p << ' ';
 
     return 0;
 }
