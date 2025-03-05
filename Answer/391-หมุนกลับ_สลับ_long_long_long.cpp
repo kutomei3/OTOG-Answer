d0ac15bc229e37b9e246239528dcc66e804da4b9
ohmusan <ohmnotbad@gmail.com>
2025-03-05 15:32:35 +0700

    Create 391-หมุนกลับ_สลับ_long_long_long.cpp

---
+++ Answer/391-หมุนกลับ_สลับ_long_long_long.cpp
@@ -0,0 +1,24 @@
+/**
+ *  Author: kutomei3
+ *  Task: หมุนกลับ สลับ long long long no.391
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    reverse(s.begin(), s.end());
+    cout << s;
+
+    return 0;
+}
+
