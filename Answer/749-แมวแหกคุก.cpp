eb280a73bd58a584d3c163b0be565a9c0c9c8079
ohmusan <ohmnotbad@gmail.com>
2025-03-05 12:58:27 +0700

    Create 749-แมวแหกคุก.cpp

---
+++ Answer/749-แมวแหกคุก.cpp
@@ -0,0 +1,27 @@
+/**
+ *  Author: kutomei3
+ *  Task: แมวแหกคุก no.749
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+
+        cout << n * m << '\n';
+    }
+
+    return 0;
+}
